94cd958e64964e84427036fa3b4e8525d60be1f6
chris <>
2004-10-15 17:57:28 +0000

    Added a full recursive descent parser that recognizes the mitchell grammar, though I have not yet done a full test suite for all the syntax bits. That should be coming shortly.
    While I hate making the first commit on something be it in a mostly
    completed state, there's really not a good halfway point to commit the
    parser.  It's all so simple and mutually recursive that this is the
    easiest way to do it.

--- src/kern/main.c
+++ src/kern/main.c
@@ -1,7 +1,7 @@
 /* The main file of the mitchell kernel, which controls the entire
  * compilation process.
  *
- * $Id: main.c,v 1.5 2004/10/13 14:02:37 chris Exp $
+ * $Id: main.c,v 1.6 2004/10/15 13:57:28 chris Exp $
  */
 
 /* mitchell - the bootstrapping compiler
@@ -24,13 +24,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "tokens.h"
+extern void parse (const char *filename);
 
 int main (int argc, char **argv)
 {
-   token_t *t;
-   FILE *in;
-
    /* Grab locale information from the environment. */
    setlocale (LC_ALL, "");
 
@@ -40,63 +37,7 @@ int main (int argc, char **argv)
       exit(1);
    }
 
-   if ((in = fopen (argv[1], "r")) == NULL)
-   {
-      fprintf (stderr, "could not open for reading: %s\n", argv[1]);
-      exit(1);
-   }
+   parse (argv[1]);
 
-   /* Read in each token of the input file and print it out. */
-   while ((t = next_token (in)) != NULL)
-   {
-      switch (t->type) {
-         case ASSIGN: printf ("ASSIGN "); break;
-         case CASE: printf ("CASE "); break;
-         case COLON: printf ("COLON "); break;
-         case COMMA: printf ("COMMA "); break;
-         case CONST: printf ("CONST "); break;
-         case DBLQUOTE: printf ("DBLQUOTE "); break;
-         case DECL: printf ("DECL "); break;
-         case DOT: printf ("DOT "); break;
-         case ELSE: printf ("ELSE "); break;
-         case END: printf ("END "); break;
-         case FUNCTION: printf ("FUNCTION "); break;
-         case IF: printf ("IF "); break;
-         case IN: printf ("IN "); break;
-         case LBRACK: printf ("LBRACK "); break;
-         case LPAREN: printf ("LPAREN "); break;
-         case MAPSTO: printf ("MAPSTO "); break;
-         case MODULE: printf ("MODULE "); break;
-         case RBRACK: printf ("RBRACK "); break;
-         case RPAREN: printf ("RPAREN "); break;
-         case THEN: printf ("THEN "); break;
-         case TYPE: printf ("TYPE "); break;
-         case VAR: printf ("VAR "); break;
-
-         case IDENTIFIER:
-            printf ("IDENTIFIER(%ls) ", t->string);
-            free (t->string);
-            break;
-
-         case INTEGER:
-            printf ("INTEGER(%li) ", t->integer);
-            break;
-
-         case STRING:
-            printf ("STRING(%ls) ", t->string);
-            free (t->string);
-            break;
-
-         case BOOLEAN:
-            printf ("BOOLEAN(%s) ", t->boolean == 1 ? "true" : "false");
-            break;
-      }
-
-      free (t);
-   }
-
-   printf ("\n");
-
-   fclose (in);
    return 0;
 }
---
+++ src/kern/parse.c
@@ -0,0 +1,578 @@
+/* The parser - recognize the grammar for mitchell by repeatedely calling the
+ * tokenizer to extract tokens from the input file, then feeding them into
+ * our recursive descent parser.  I am purposefully avoiding using a parser
+ * generator like yacc because I don't want to have to rely on things I
+ * won't have when it comes time to bootstrap.
+ *
+ * I don't know of any ambiguities in this grammar, though I have not
+ * gone through and thoroughly checked.  The grammar itself may be seen
+ * in mitchell/docs/grammar, though that file is not really any more
+ * descriptive than this one.
+ *
+ * $Id: parse.c,v 1.1 2004/10/15 13:57:28 chris Exp $
+ */
+
+/* mitchell - the bootstrapping compiler
+ * Copyright (C) 2004 Chris Lumens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tokens.h"
+
+static FILE *in;           /* the input file */
+static token_t *tok;       /* lookahead token - not yet examined */
+
+/* These functions are seriously mutually recursive, so put forward
+ * declarations of them here.
+ */
+static void parse_branch_expr();
+static void parse_branch_lst();
+static void parse_case_expr();
+static void parse_const_decl();
+static void parse_const_decl_proto();
+static void parse_decl();
+static void parse_decl_expr();
+static void parse_decl_lst();
+static void parse_expr();
+static void parse_expr_lst();
+static void parse_fun_call_or_id();
+static void parse_fun_decl();
+static void parse_fun_decl_proto();
+static void parse_id();
+static void parse_id_lst();
+static void parse_if_expr();
+static void parse_module_decl();
+static void parse_module_decl_lst();
+static void parse_proto();
+static void parse_proto_lst();
+static void parse_record_assn_lst();
+static void parse_single_ty();
+static void parse_ty();
+static void parse_ty_decl();
+static void parse_ty_decl_proto();
+static void parse_var_decl();
+static void parse_var_decl_proto();
+
+/* Check the type of the lookahead token.  If we match, read the next
+ * token so we'll be ready to check again in the future.  If we don't
+ * match, that's a parse error and we fail.  No clever error correction
+ * here.
+ */
+static void match (const unsigned int type)
+{
+   if (tok == NULL)
+   {
+      fprintf (stderr, "parse error:  premature end of input file\n");
+      exit (1);
+   }
+
+   if (tok->type == type)
+   {
+      printf ("  ate %s\n", token_map[type]);
+      tok = next_token (in);
+   }
+   else
+   {
+      fprintf (stderr, "Parse error on line %d: ", tok->lineno);
+      fprintf (stderr, "expected %s, but read %s instead\n",
+               token_map[type], token_map[tok->type]);
+      exit (1);
+   }
+}
+
+/* Main parser entry point - open the file, read the first token, and try
+ * to parse the start symbol.
+ */
+void parse (const char *filename)
+{
+   if ((in = fopen (filename, "r")) == NULL)
+   {
+      fprintf (stderr, "could not open for reading: %s\n", filename);
+      exit(1);
+   }
+
+   tok = next_token (in);
+   parse_module_decl_lst();
+   match (ENDOFFILE);
+
+   fclose (in);
+}
+
+/* branch-expr ::= id
+ *               | INTEGER
+ *               | STRING
+ *               | BOOLEAN
+ */
+static void parse_branch_expr()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   switch (tok->type) {
+      case INTEGER:
+         match(INTEGER);
+         break;
+
+      case STRING:
+         match(STRING);
+         break;
+
+      case BOOLEAN:
+         match(BOOLEAN);
+         break;
+
+      case IDENTIFIER:
+         parse_id();
+         break;
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* branch-lst ::= branch-expr MAPSTO expr
+ *              | branch-expr MAPSTO expr COMMA branch-lst
+ */
+static void parse_branch_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_branch_expr();
+   match(MAPSTO);
+   parse_expr();
+
+   if (tok != NULL && tok->type == COMMA)
+   {
+      match(COMMA);
+      parse_branch_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* case-expr ::= CASE expr IN branch-lst END */
+static void parse_case_expr()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(CASE);
+   parse_expr();
+   match(IN);
+   parse_branch_lst();
+   match(END);
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* const-decl ::= const-decl-proto ASSIGN expr */
+static void parse_const_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_const_decl_proto();
+   match(ASSIGN);
+   parse_expr();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* const-decl-proto ::= CONST IDENTIFIER COLON ty */
+static void parse_const_decl_proto()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(CONST);
+   match(IDENTIFIER);
+   match(COLON);
+   parse_ty();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* decl ::= ty-decl
+ *        | var-decl
+ *        | const-decl
+ *        | fun-decl
+ */
+static void parse_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   switch (tok->type) {
+      case TYPE:
+         parse_ty_decl();
+         break;
+
+      case VAR:
+         parse_var_decl();
+         break;
+
+      case CONST:
+         parse_const_decl();
+         break;
+         
+      case FUNCTION:
+         parse_fun_decl();
+         break;
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* decl-expr ::= DECL decl-lst IN expr END */
+static void parse_decl_expr()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(DECL);
+   parse_decl_lst();
+   match(IN);
+   parse_expr();
+   match(END);
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* decl-lst ::= decl decl-lst
+ *            | decl
+ */
+static void parse_decl_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   if (tok != NULL && (tok->type == TYPE || tok->type == VAR ||
+                       tok->type == CONST || tok->type == FUNCTION))
+   {
+      parse_decl();
+      parse_decl_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* expr ::= LBRACK expr-lst RBRACK
+ *        | LBRACE record-assn-lst RBRACE
+ *        | case-expr
+ *        | decl-expr
+ *        | if-expr
+ *        | fun-call-or-id
+ *        | INTEGER
+ *        | STRING
+ *        | BOOLEAN
+ */
+static void parse_expr()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   switch (tok->type) {
+      case LBRACK:
+         match(LBRACK);
+         parse_expr_lst();
+         match(RBRACK);
+         break;
+
+      case LBRACE:
+         match(LBRACE);
+         parse_record_assn_lst();
+         match(RBRACE);
+         break;
+
+      case CASE:
+         parse_case_expr();
+         break;
+
+      case DECL:
+         parse_decl_expr();
+         break;
+
+      case IF:
+         parse_if_expr();
+         break;
+
+      case IDENTIFIER:
+         parse_fun_call_or_id();
+         break;
+
+      case INTEGER:
+         match(INTEGER);
+         break;
+
+      case STRING:
+         match(STRING);
+         break;
+
+      case BOOLEAN:
+         match(BOOLEAN);
+         break;
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* expr-lst ::= expr COMMA expr-lst
+ *            | expr
+ */
+static void parse_expr_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_expr();
+
+   if (tok != NULL && tok->type == COMMA)
+   {
+      match(COMMA);
+      parse_expr_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* fun-call-or-id ::= id LPAREN expr-lst RPAREN
+ *                  | id LPAREN RPAREN
+ *                  | id
+ */
+static void parse_fun_call_or_id()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_id();
+
+   if (tok != NULL && tok->type == LPAREN)
+   {
+      match(LPAREN);
+
+      if (tok != NULL && tok->type != RPAREN)
+      {
+         parse_expr_lst();
+         match(RPAREN);
+      }
+      else
+         match(RPAREN);
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* fun-decl ::= fun-decl-proto ASSIGN expr */
+static void parse_fun_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_fun_decl_proto();
+   match(ASSIGN);
+   parse_expr();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* fun-decl-proto ::= FUNCTION IDENTIFIER COLON ty LPAREN id-lst RPAREN */
+static void parse_fun_decl_proto()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(FUNCTION);
+   match(IDENTIFIER);
+   match(COLON);
+   parse_ty();
+   match(LPAREN);
+   parse_id_lst();
+   match(RPAREN);
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* id ::= IDENTIFIER
+ *      | IDENTIFIER DOT id
+ */
+static void parse_id()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(IDENTIFIER);
+
+   if (tok != NULL && tok->type == DOT)
+   {
+      match(DOT);
+      parse_id();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* id-lst ::= IDENTIFIER COLON ty
+ *          | IDENTIFIER COLON ty COMMA id-lst
+ */
+static void parse_id_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(IDENTIFIER);
+   match(COLON);
+   parse_ty();
+
+   if (tok != NULL && tok->type == COMMA)
+   {
+      match(COMMA);
+      parse_id_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* if-expr ::= IF expr THEN expr ELSE expr */
+static void parse_if_expr()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(IF);
+   parse_expr();
+   match(THEN);
+   parse_expr();
+   match(ELSE);
+   parse_expr();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* module-decl ::= MODULE IDENTIFIER ASSIGN DECL proto-lst IN decl-lst END */
+static void parse_module_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match (MODULE);
+   match (IDENTIFIER);
+   match (ASSIGN);
+   match (DECL);
+   parse_proto_lst();
+   match (IN);
+   parse_decl_lst();
+   match (END);
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* module-decl-lst ::= module-decl
+ *                   | module-decl module-decl-lst
+ */
+static void parse_module_decl_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   if (tok != NULL && tok->type == MODULE)
+   {
+      parse_module_decl();
+      parse_module_decl_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* proto ::= ty-decl-proto
+ *         | var-decl-proto
+ *         | const-decl-proto
+ *         | fun-decl-proto
+ */
+static void parse_proto()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   switch (tok->type) {
+      case TYPE:
+         parse_ty_decl_proto();
+         break;
+
+      case VAR:
+         parse_var_decl_proto();
+         break;
+
+      case CONST:
+         parse_const_decl_proto();
+         break;
+
+      case FUNCTION:
+         parse_fun_decl_proto();
+         break;
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* proto-lst ::= proto
+ *             | proto proto-lst
+ */
+static void parse_proto_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   if (tok != NULL && (tok->type == TYPE || tok->type == VAR ||
+                       tok->type == CONST || tok->type == FUNCTION))
+   {
+      parse_proto();
+      parse_proto_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* record-assn-lst ::= IDENTIFIER ASSIGN expr
+ *                   | IDENTIFIER ASSIGN expr COMMA record-assn-lst
+ */
+static void parse_record_assn_lst()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(IDENTIFIER);
+   match(ASSIGN);
+   parse_expr();
+
+   if (tok != NULL && tok->type == COMMA)
+   {
+      match(COMMA);
+      parse_record_assn_lst();
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* single-ty ::= LBRACK id-lst RBRACK
+ *             | id
+ */
+static void parse_single_ty()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   switch (tok->type) {
+      case LBRACK:
+         match(LBRACK);
+         parse_id_lst();
+         match(RBRACK);
+         break;
+
+      case IDENTIFIER:
+         parse_id();
+         break;
+   }
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* ty ::= single-ty
+ *      | single-ty LIST
+ */
+static void parse_ty()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_single_ty();
+   
+   if (tok != NULL && tok->type == LIST)
+      match(LIST);
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* ty-decl ::= ty-decl-proto ASSIGN ty */
+static void parse_ty_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_ty_decl_proto();
+   match(ASSIGN);
+   parse_ty();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* ty-decl-proto ::= TYPE id */
+static void parse_ty_decl_proto()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(TYPE);
+   parse_id();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* var-decl ::= var-decl-proto ASSIGN expr */
+static void parse_var_decl()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   parse_var_decl_proto();
+   match(ASSIGN);
+   parse_expr();
+   printf ("leaving %s\n", __FUNCTION__);
+}
+
+/* var-decl-proto ::= VAR IDENTIFIER COLON ty */
+static void parse_var_decl_proto()
+{
+   printf ("entering %s\n", __FUNCTION__);
+   match(VAR);
+   match(IDENTIFIER);
+   match(COLON);
+   parse_ty();
+   printf ("leaving %s\n", __FUNCTION__);
+}
