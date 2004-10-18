a21cc8368dfc19b1b2d4438e44cf939a678fb638
chris <>
2004-10-18 21:01:38 +0000

    Added much better command line argument processing through getopt and a compiler control structure that holds all the runtime settings.  The basis of the debugging framework is also in place with the -Idebug-parser command line option, though this option does not currently do anything.

---
+++ src/include/config.h
@@ -0,0 +1,40 @@
+/* This file defines the structures that hold all the runtime configuration
+ * of the compiler.  For the most part, this configuration is set by the
+ * command line options provided by the user.
+ *
+ * $Id: config.h,v 1.1 2004/10/18 17:01:36 chris Exp $
+ */
+
+/* mitchell - the bootstrapping compiler
+ * Copyright (C) 2004 Chris Lumens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef _CONFIG_H
+#define _CONFIG_H 1
+
+typedef struct {
+   char *filename;                     /* the file we're compiling */
+
+   struct {
+      unsigned int parser_debug;       /* debug output level for the parser */
+   } debug;
+} compiler_config_t;
+
+/* Here's the one instance of this structure we need. */
+extern compiler_config_t compiler_config;
+
+#endif
+
+/* vim: set tags=../tags: */
--- src/kern/main.c
+++ src/kern/main.c
@@ -1,7 +1,7 @@
 /* The main file of the mitchell kernel, which controls the entire
  * compilation process.
  *
- * $Id: main.c,v 1.8 2004/10/15 14:59:55 chris Exp $
+ * $Id: main.c,v 1.9 2004/10/18 17:01:38 chris Exp $
  */
 
 /* mitchell - the bootstrapping compiler
@@ -20,24 +20,122 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+#include <getopt.h>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "config.h"
 #include "parse.h"
+
+/* Defines for what we want getopt_long to return. */
+#define OPT_HELP                 100
+#define OPT_VERBOSE_HELP         101
+#define OPT_VERSION              102
+#define OPT_IDEBUG_PARSER        103
+
+/* The command line arguments we accept. */
+static char *shortopts = "hv";
+
+static struct option longopts[] = {
+   /* Help options */
+   { "help", 0, NULL, OPT_HELP },
+   { "verbose-help", 0, NULL, OPT_VERBOSE_HELP },
+   { "version", 0, NULL, OPT_VERSION },
+
+   /* Internal compiler options */
+   { "Idebug-parser", 1, NULL, OPT_IDEBUG_PARSER },
+
+   { 0, 0, 0, 0 }
+};
+
+/* Stash all the command line arguments in here. */
+compiler_config_t compiler_config = { .debug.parser_debug = 0 };
+
+static void help_internal_debug ()
+{
+   printf ("Internal Debugging Options:\n");
+   printf ("-Idebug-parser=N\t\tSet debugging output level for the tokenizer and parser\n");
+}
+
+static void verbose_help (const char *progname)
+{
+   printf ("usage:  %s <filename>\n", progname);
+   help_internal_debug();
+   exit (0);
+}
+
+static void help (const char *progname)
+{
+   printf ("usage:  %s <filename>\n", progname);
+   printf ("\t-verbose-help for a list of all command line arguments\n");
+   exit (0);
+}
+
+static void version (const char *progname)
+{
+   printf ("%s version WHATEVER\n(C) 2004 Chris Lumens\n", progname);
+   exit (0);
+}
+
+static void handle_arguments (int argc, char **argv)
+{
+   int index, retval;
+
+   if (argc == 1)
+      help (argv[0]);
+
+   while ((retval = getopt_long_only (argc, argv, shortopts, longopts,
+                                      &index)) != -1)
+   {
+      switch (retval) {
+         case 'h':
+         case OPT_HELP:
+            help (argv[0]);
+            break;
+
+         case OPT_VERBOSE_HELP:
+            verbose_help (argv[0]);
+            break;
+
+         case 'v':
+         case OPT_VERSION:
+            version (argv[0]);
+            break;
+
+         case OPT_IDEBUG_PARSER:
+            if (optarg)
+               compiler_config.debug.parser_debug = atoi(optarg);
+            else
+            {
+               printf ("-Idebug-parser requires an argument\n");
+               exit(1);
+            }
+
+            break;
+
+         /* getopt already told us what was wrong so only print the help. */
+         default:
+            help(argv[0]);
+            break;
+
+      }
+   }
+
+   if (optind+1 == argc)
+      compiler_config.filename = strdup (argv[optind]);
+   else
+      help (argv[0]);
+}
 
 int main (int argc, char **argv)
 {
    /* Grab locale information from the environment. */
    setlocale (LC_ALL, "");
 
-   if (argc != 2)
-   {
-      fprintf (stderr, "usage: %s <file>\n", argv[0]);
-      exit(1);
-   }
-
-   parse (argv[1]);
+   handle_arguments (argc, argv);
+   parse (compiler_config.filename);
 
    return 0;
 }
