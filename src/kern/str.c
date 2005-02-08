d572cbaeeb3904fbfa6ba76ef620c8542d23c8ad
chris <>
2005-02-08 09:28:32 +0000

    Added a generic string handling library.  Only one thing in there right now but that'll increase as I look through the rest of the source.  I can't do that right now due to other pending commits.

---
+++ src/kern/str.c
@@ -0,0 +1,58 @@
+/* Generic string handling functions.
+ *
+ * $Id: str.c,v 1.1 2005/02/08 04:28:32 chris Exp $
+ */
+
+/* mitchell - the bootstrapping compiler
+ * Copyright (C) 2004 Chris Lumens
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#define _GNU_SOURCE
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+#include "basic_types.h"
+#include "memory.h"
+#include "str.h"
+
+#define WCSLEN(str)  (wcslen(str)*sizeof(wchar_t))
+
+wchar_t *build_wcsstr (int nargs, ...)
+{
+   va_list ap;
+   unsigned int i;
+   wchar_t *retval = NULL;
+
+   if (nargs == 0)
+      return NULL;
+
+   va_start (ap, nargs);
+
+   retval = wcsdup(va_arg(ap, wchar_t *));
+
+   for (i = 1; i < nargs; i++)
+   {
+      wchar_t *next = va_arg(ap, wchar_t *);
+
+      REALLOC (retval, WCSLEN(retval)+WCSLEN(next));
+      retval = wcscat (retval, next);
+   }
+
+   va_end (ap);
+   return retval;
+}
