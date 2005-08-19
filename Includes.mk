e2713d0a5225e8f1d1fb8288b0569a98b7711327
chris <>
2005-08-19 05:14:34 +0000

    Don't include optimization flags when debugging since gdb can't handle some of the things gcc can do (inlining certain static functions, for instance).

--- Includes.mk
+++ Includes.mk
@@ -1,6 +1,9 @@
-# $Id: Includes.mk,v 1.1 2005/07/07 05:04:02 chris Exp $
+# $Id: Includes.mk,v 1.2 2005/08/19 01:14:31 chris Exp $
 
 # Set these to your liking on the command line, or accept the defaults.
 PREFIX ?= /usr/local
 BINDIR ?= $(PREFIX)/bin
 LOCALEDIR ?= $(PREFIX)/locale
+
+# Flags for optimization - not set if debugging
+OPTFLAGS = -O2
